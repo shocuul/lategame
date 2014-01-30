88e7d481e57fe5a94c79ee4f29bd201026e48c8a
Jose David <josepdvahn@me.com>
2014-01-30 16:16:11 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lategame::Application.config.secret_key_base = '8a94cd47b0de26a9a14e83b4555b5a3f6980d480e87812531fb6dd0b4fa68ea97c1da0324ed2104076a8344fce7c53919fb0d473b4c086adb525c3c0bf420ae2'
